8b73bfeeb27c539d19f6369577cac506d4345007
gabrieeel <gabrielp@gmail.com>
2024-08-23 21:18:20 -0300

    Change notify method to be async. If message is sent successfully, update 'notified' attribute. Add more log in case of errors

--- lib/notifier.py
+++ lib/notifier.py
@@ -1,8 +1,11 @@
 import telegram
+import traceback
 import logging
 import random
 from lib.sslless_session import SSLlessSession
 import yaml
+from telegram.constants import ParseMode
+import sqlite3
 
 class NullNotifier:
     def notify(self, properties):
@@ -18,16 +21,28 @@ class Notifier(NullNotifier):
             self.bot = telegram.Bot(token=self.config['token'])
         
 
-    def notify(self, properties):
+    async def notify(self, properties):
         logging.info(f'Notifying about {len(properties)} properties')
         text = random.choice(self.config['messages'])
-        self.bot.send_message(chat_id=self.config['chat_id'], text=text)
+        try:
+            await self.bot.send_message(chat_id=self.config['chat_id'], text=text)
+            for prop in properties:
+                try:
+                    logging.info(f"Notifying about {prop['url']}")
+                    message = await self.bot.send_message(chat_id=self.config['chat_id'], 
+                            text=f"[{prop['title']}]({prop['url']})",
+                            parse_mode=ParseMode.MARKDOWN)
+                    if message:
+                        prop['notified'] = 1
+                except Exception as msgEx:
+                    logging.error(f"Notification failed for property: {prop['url']}")
+                    logging.error(msgEx)
+                                
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            logging.error(f"Notification failed for the following properties:")
+            logging.error('\n'.join(str(p['url']) for p in properties))
 
-        for prop in properties:
-            logging.info(f"Notifying about {prop['url']}")
-            self.bot.send_message(chat_id=self.config['chat_id'], 
-                    text=f"[{prop['title']}]({prop['url']})",
-                    parse_mode=telegram.ParseMode.MARKDOWN)
 
     def test(self, message):
         self.bot.send_message(chat_id=self.config['chat_id'], text=message)
